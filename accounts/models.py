b28a9eb3c1e42eda1a9956badcc791c52b104b4f
Chaima Ben Jaafar <chaima.benjaafar.chaima@gmail.com>
2023-10-13 18:58:51 +0100

    initial Auth

---
+++ accounts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password = None, **kwargs):
+        if not email:
+            raise ValueError("Users must have email address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using= self._db)
+        return user
+        
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname', 'lastname']
+
+    def get_full_name(self):
+        return self.firstname
+
+    def __str__(self):
+        return self.email
+    
