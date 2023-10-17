cbe4e106796fb4bf5decb6980cd60c6ead07e14b
Chaima Ben Jaafar <chaima.benjaafar.chaima@gmail.com>
2023-10-17 12:46:12 +0100

    updating admin panel

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
 
 from .models import UserAccount
-<<<<<<< HEAD
-=======
 from .models import Graph
 from .models import Guest
->>>>>>> chaima_dev
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('firstname', 'email','date_joined')  # Adjust this list according to your model fields
 
-<<<<<<< HEAD
-
-admin.site.register(UserAccount,UserAdmin)
-=======
 class GuestAdmin(admin.ModelAdmin):
     list_display = ('ip_address','visit_counter')
 
@@ -23,4 +16,3 @@ class GraphAdmin(admin.ModelAdmin):
 admin.site.register(UserAccount,UserAdmin)
 admin.site.register(Graph,GraphAdmin)
 admin.site.register(Guest,GuestAdmin)
->>>>>>> chaima_dev
