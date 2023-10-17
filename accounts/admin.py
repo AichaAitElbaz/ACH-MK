9e5f370a45a2b4589c8aef6546c85302bf0c59b8
Chaima Ben Jaafar <chaima.benjaafar.chaima@gmail.com>
2023-10-17 11:28:54 +0100

    adding graph model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
 from .models import UserAccount
+from .models import Graph
+from .models import Guest
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('firstname', 'email','date_joined')  # Adjust this list according to your model fields
 
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('ip_address','visit_counter')
+
+class GraphAdmin(admin.ModelAdmin):
+    list_display = ('source_file','graph','interpretation')
 
 admin.site.register(UserAccount,UserAdmin)
+admin.site.register(Graph,GraphAdmin)
+admin.site.register(Guest,GuestAdmin)
