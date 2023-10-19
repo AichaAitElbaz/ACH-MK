28202fa46008c6ac28fbc3b91388e08d041e7a64
Kaoutar <kaoutarmghar@gmail.com>
2023-10-19 21:29:27 +0100

    Interface

---
+++ FRONTEND/src/assets/index.js
@@ -0,0 +1,27 @@
+import logo from "./Logo.png";
+import menu from "./menu.png";
+import close from "./close.png";
+import robot from "./robot.png";
+import discount from "./Discount.png";
+import arrowUp from "./arrow-up.png";
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import twitter from "./twitter.png";
+import updown from "./up-down-Arrow.svg";
+import down from "./down-Arrow.svg";
+
+export {
+    logo,
+    menu,
+    close,
+    robot,
+    discount,
+    arrowUp,
+    facebook,
+    instagram,
+    twitter,
+    linkedin,
+    updown,
+    down
+};
---
+++ FRONTEND/src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import GenerateForm from "./GenerateForm";
+
+export {
+  Navbar,
+  Footer,
+  Hero,
+  GetStarted,
+  GenerateForm
+};
